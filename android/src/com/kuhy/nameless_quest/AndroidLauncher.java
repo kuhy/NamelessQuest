d6f5262aa7e87f7dd08b8e939f4ebf18529348d9
Ondrej Kuhejda <ondrejkuhejda@gmail.com>
2017-06-10 17:40:00 +0200

    hidden navigation bar

--- android/src/com/kuhy/nameless_quest/AndroidLauncher.java
+++ android/src/com/kuhy/nameless_quest/AndroidLauncher.java
@@ -1,6 +1,8 @@
 package com.kuhy.nameless_quest;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -10,6 +12,41 @@ public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		int currentApiVersion = android.os.Build.VERSION.SDK_INT;
+
+		final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+				| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+				| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+				| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+				| View.SYSTEM_UI_FLAG_FULLSCREEN
+				| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+		// This work only for android 4.4+
+		if(currentApiVersion >= Build.VERSION_CODES.KITKAT)
+		{
+
+			getWindow().getDecorView().setSystemUiVisibility(flags);
+
+			// Code below is to handle presses of Volume up or Volume down.
+			// Without this, after pressing volume buttons, the navigation bar will
+			// show up and won't hide
+			final View decorView = getWindow().getDecorView();
+			decorView
+					.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener()
+					{
+
+						@Override
+						public void onSystemUiVisibilityChange(int visibility)
+						{
+							if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0)
+							{
+								decorView.setSystemUiVisibility(flags);
+							}
+						}
+					});
+		}
+
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useAccelerometer = false;
 		config.useCompass = false;
