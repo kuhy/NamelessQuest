682723325802529e3086b23b80d29765b98d3fbe
Ondrej Kuhejda <ondrejkuhejda@gmail.com>
2017-06-19 12:45:30 +0200

    better fullscreen

--- android/src/com/kuhy/nameless_quest/AndroidLauncher.java
+++ android/src/com/kuhy/nameless_quest/AndroidLauncher.java
@@ -9,11 +9,13 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.kuhy.nameless_quest.NamelessQuest;
 
 public class AndroidLauncher extends AndroidApplication {
+	int currentApiVersion;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		int currentApiVersion = android.os.Build.VERSION.SDK_INT;
+		currentApiVersion = android.os.Build.VERSION.SDK_INT;
 
 		final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 				| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -51,5 +53,21 @@ public class AndroidLauncher extends AndroidApplication {
 		config.useAccelerometer = false;
 		config.useCompass = false;
 		initialize(new NamelessQuest(), config);
+	}
+
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus)
+	{
+		super.onWindowFocusChanged(hasFocus);
+		if(currentApiVersion >= Build.VERSION_CODES.KITKAT && hasFocus)
+		{
+			getWindow().getDecorView().setSystemUiVisibility(
+					View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+							| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+							| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+							| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+							| View.SYSTEM_UI_FLAG_FULLSCREEN
+							| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+		}
 	}
 }
